afac27d4ec128c96e85ebbd08d0639672a86e991
rafaelfs96 <rafaelfs96@outlook.com>
2020-03-24 16:58:24 -0300

    change conditional statement

--- src/isViewport/isInViewport.ts
+++ src/isViewport/isInViewport.ts
@@ -4,13 +4,23 @@ export function isInViewport(element: HTMLElement, part: string = 'any'): boolea
   const windowHeight: number = (window.innerHeight || document.documentElement.clientHeight)
   const windowWidth: number = (window.innerWidth || document.documentElement.clientWidth)
 
-  if('any' === part) {
-    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0)
-    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0)
+  let result: boolean
 
-    return (vertInView && horInView)
+  switch(part) {
+    case 'any':
+      const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0)
+      const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0)
+  
+      result = (vertInView && horInView)
+      break
+    
+    case 'complete':
+      result = (rect.top >= 0 && rect.left >= 0 && rect.right <= windowWidth && rect.bottom <= windowHeight)
+      break
 
-  } else if ('complete' === part) {
-    return (rect.top >= 0 && rect.left >= 0 && rect.right <= windowWidth && rect.bottom <= windowHeight)
-  } else return null
+    default:
+      result = null
+  }
+
+  return result
 }
